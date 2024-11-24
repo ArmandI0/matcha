6575d7162f88d3601b32abe56b10505f80519f00
Your Name <you@example.com>
2024-11-24 15:02:44 +0100

    Feat : script for React

--- frontend/website-react/script_config.sh
+++ frontend/website-react/script_config.sh
@@ -1,9 +1,14 @@
 # Si pas installe 
-sudo apt install npm
+if ! command -v npm &> /dev/null
+then
+    sudo apt install npm
+else
+    echo "npm already install"
 
 # pour installer les dependences sur pc
 # Faire un script pour les installer sur les nouveaux projet 
 
-# MUI
+# MUI dependencies
+
 npm install @mui/material @emotion/react @emotion/styled
 npm install @mui/icons-material
