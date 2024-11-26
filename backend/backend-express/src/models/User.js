d2bfd4e8686c45c5bf40d56b858755219de53c8f
Your Name <you@example.com>
2024-11-26 22:19:10 +0100

    feat : create table user and user profile

--- backend/backend-express/src/models/User.js
+++ backend/backend-express/src/models/User.js
@@ -2,14 +2,84 @@ const { DataTypes } = require('sequelize');
 const database = require('../config/database');
 
 const User = database.define('User', {
-  
-  username: {
-    type: DataTypes.STRING
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
   },
+
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+
   email: {
     type: DataTypes.STRING,
-    unique: true
+    unique: true,
+  },
+
+  password: {
+    type: DataTypes.STRING,
   }
 });
 
+const UserProfile = database.define('UserProfile', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  firstname: {
+    type: DataTypes.STRING,
+  },
+
+  lastname: {
+    type: DataTypes.STRING,
+  },
+
+  username: {
+    type: DataTypes.STRING,
+    references: {
+      model: 'Users',
+      key: 'username',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+    unique: true,
+  },
+
+  gender: {
+    type: DataTypes.STRING,
+  },
+
+  sexualPreference: {
+    type: DataTypes.STRING,
+  },
+
+  sexualPreference: {
+    type: DataTypes.STRING,
+  },
+
+  biography: {
+    type: DataTypes.TEXT,
+  },
+
+  //ajouter list of preference
+
+});
+
 module.exports = User;
+module.exports = UserProfile;
+
+
+// userId: {
+//   type: DataTypes.INTEGER,
+//   references: {
+//     model: 'Users', // Le nom de la table associée
+//     key: 'id', // La colonne clé primaire à laquelle se réfère cette clé étrangère
+//   },
+//   onUpdate: 'CASCADE', // Que faire lors d'une mise à jour
+//   onDelete: 'SET NULL', // Que faire lors d'une suppression
+// },
+// });
